fcea819ec038d6e77ecec9190c036195e522e073
salmon99 <namhj99@naver.com>
2020-07-20 14:52:41 +0900

    Create MainActivity.java

---
+++ app/src/main/java/com/example/togethersujung2020/ui/main/MainActivity.java
@@ -0,0 +1,17 @@
+//커뮤니티 메인 화면
+
+package com.example.togethersujung2020.ui.main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.togethersujung2020.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
