d0ea5aeece1574e51ba646d63a29972a3364af02
salmon99 <namhj99@naver.com>
2020-07-23 05:23:37 +0900

    FreeActivity에 리스트뷰 코드 작성

--- app/src/main/java/com/example/togethersujung2020/ui/freeBoard/FreeActivity.java
+++ app/src/main/java/com/example/togethersujung2020/ui/freeBoard/FreeActivity.java
@@ -1,9 +1,10 @@
 package com.example.togethersujung2020.ui.freeBoard;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +14,25 @@ public class FreeActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.acrivity_free);
+        setContentView(R.layout.activity_free);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU) ;
+
+        ListView listview = (ListView) findViewById(R.id.listview1) ;
+        listview.setAdapter(adapter) ;
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+
+                // get TextView's Text.
+                String strText = (String) parent.getItemAtPosition(position) ;
+
+                // TODO : use strText
+            }
+        }) ;
     }
+    static final String[] LIST_MENU = {"LIST1", "LIST2", "LIST3"} ;
+
 }
+
