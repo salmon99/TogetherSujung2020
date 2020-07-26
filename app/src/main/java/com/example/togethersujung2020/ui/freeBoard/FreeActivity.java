46ee85f08716c09282c1e8a0c363aac1c2e0f613
salmon99 <namhj99@naver.com>
2020-07-26 18:36:34 +0900

    사진, 제목, 댓글수를 보여주는 커스텀 리스트뷰 작성

--- app/src/main/java/com/example/togethersujung2020/ui/freeBoard/FreeActivity.java
+++ app/src/main/java/com/example/togethersujung2020/ui/freeBoard/FreeActivity.java
@@ -1,5 +1,6 @@
 package com.example.togethersujung2020.ui.freeBoard;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -7,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.togethersujung2020.R;
 
@@ -16,23 +18,41 @@ public class FreeActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU) ;
+        ListView listview ;
+        FreeListViewAdapter adapter;
 
-        ListView listview = (ListView) findViewById(R.id.listview1) ;
-        listview.setAdapter(adapter) ;
+        // Adapter 생성
+        adapter = new FreeListViewAdapter() ;
 
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) findViewById(R.id.listview1);
+        listview.setAdapter(adapter);
+
+        //todo: 데이터를 데이터베이스로부터 받아와야함
+
+        // 첫 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.profile),
+                "Box", "Account Box Black 36dp") ;
+        // 두 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.user),
+                "Circle", "Account Circle Black 36dp") ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.user2),
+                "Ind", "Assignment Ind Black 36dp") ;
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
+            @Override
             public void onItemClick(AdapterView parent, View v, int position, long id) {
+                // get item
+                FreeListViewItem item = (FreeListViewItem) parent.getItemAtPosition(position) ;
 
-                // get TextView's Text.
-                String strText = (String) parent.getItemAtPosition(position) ;
+                String titleStr = item.getTitle() ;
+                String descStr = item.getDesc() ;
+                Drawable iconDrawable = item.getIcon() ;
 
-                // TODO : use strText
+                // TODO : use item data.
             }
         }) ;
     }
-    static final String[] LIST_MENU = {"LIST1", "LIST2", "LIST3"} ;
 
 }
 
---
+++ app/src/main/java/com/example/togethersujung2020/ui/freeBoard/FreeListViewItem.java
@@ -0,0 +1,29 @@
+package com.example.togethersujung2020.ui.freeBoard;
+
+import android.graphics.drawable.Drawable;
+
+public class FreeListViewItem {
+    private Drawable iconDrawable ;
+    private String titleStr ;
+    private String descStr ;
+
+    public void setIcon(Drawable icon) {
+        iconDrawable = icon ;
+    }
+    public void setTitle(String title) {
+        titleStr = title ;
+    }
+    public void setDesc(String desc) {
+        descStr = desc ;
+    }
+
+    public Drawable getIcon() {
+        return this.iconDrawable ;
+    }
+    public String getTitle() {
+        return this.titleStr ;
+    }
+    public String getDesc() {
+        return this.descStr ;
+    }
+}
