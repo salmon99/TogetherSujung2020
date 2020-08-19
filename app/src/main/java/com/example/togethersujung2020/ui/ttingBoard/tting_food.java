045bf0923e1475d42f5c8112c81db4ef31166b46
salmon99 <namhj99@naver.com>
2020-08-19 23:10:16 +0900

    띵 게시판 완성

--- app/src/main/java/com/example/togethersujung2020/ui/ttingBoard/tting_food.java
+++ app/src/main/java/com/example/togethersujung2020/ui/ttingBoard/tting_food.java
@@ -3,16 +3,27 @@ package com.example.togethersujung2020.ui.ttingBoard;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 
+import com.example.togethersujung2020.BuildConfig;
 import com.example.togethersujung2020.R;
 import com.example.togethersujung2020.ui.freeBoard.FreeActivity;
 import com.example.togethersujung2020.ui.main.MainActivity;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -45,68 +56,130 @@ public class tting_food extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tting_food, container, false);
-        Button free_board1 = (Button) view.findViewById(R.id.한식) ;
+        ListView listview = null;
+        final TtingListViewAdapter adapter;
+
+        // Adapter 생성
+        adapter = new TtingListViewAdapter();
+
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) view.findViewById(R.id.listview1);
+        listview.setAdapter(adapter);
+
+        final DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        database.child("ttingboard").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                TtingBoard board = snapshot.getValue(TtingBoard.class);
+                adapter.addItem(board.getTitle(), board.getContent(), board.getDiv(), snapshot.getKey());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                adapter.notifyDataSetChanged();
+            }
+        }); //글 목록 데이터베이스와 리스트뷰 연결
+
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                // get item
+                TtingListViewItem item = (TtingListViewItem) parent.getItemAtPosition(position);
+
+                String titleStr = item.getTitle();
+                String descStr = item.getDesc();
+                String divStr = item.getDiv();
+                String key = item.getKey();
+
+                Intent postview = new Intent(getActivity(), TtingPostViewActivity.class);
+                postview.putExtra("title", titleStr);
+                postview.putExtra("content", descStr);
+                postview.putExtra("div", divStr);
+                postview.putExtra("postkey", key);
+                startActivity(postview);
+            }
+        });
+
+
+        Button free_board1 = (Button) view.findViewById(R.id.한식);
         free_board1.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), Button1Activity.class) ;
-                startActivity(intent) ;
+                Intent intent = new Intent(getActivity(), Button1Activity.class);
+                startActivity(intent);
             }
         });
-        Button free_board2 = (Button) view.findViewById(R.id.중식) ;
+        Button free_board2 = (Button) view.findViewById(R.id.중식);
         free_board2.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), Button2Activity.class) ;
-                startActivity(intent) ;
+                Intent intent = new Intent(getActivity(), Button2Activity.class);
+                startActivity(intent);
             }
         });
-        Button free_board3 = (Button) view.findViewById(R.id.일식) ;
+        Button free_board3 = (Button) view.findViewById(R.id.일식);
         free_board3.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), Button3Activity.class) ;
-                startActivity(intent) ;
+                Intent intent = new Intent(getActivity(), Button3Activity.class);
+                startActivity(intent);
             }
         });
-        Button free_board4 = (Button) view.findViewById(R.id.양식) ;
+        Button free_board4 = (Button) view.findViewById(R.id.양식);
         free_board4.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), Button4Activity.class) ;
-                startActivity(intent) ;
+                Intent intent = new Intent(getActivity(), Button4Activity.class);
+                startActivity(intent);
             }
         });
-        Button free_board5 = (Button) view.findViewById(R.id.치킨) ;
+        Button free_board5 = (Button) view.findViewById(R.id.치킨);
         free_board5.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), Button5Activity.class) ;
-                startActivity(intent) ;
+                Intent intent = new Intent(getActivity(), Button5Activity.class);
+                startActivity(intent);
             }
         });
-        Button free_board6 = (Button) view.findViewById(R.id.피자) ;
+        Button free_board6 = (Button) view.findViewById(R.id.피자);
         free_board6.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), Button6Activity.class) ;
-                startActivity(intent) ;
+                Intent intent = new Intent(getActivity(), Button6Activity.class);
+                startActivity(intent);
             }
         });
-        Button free_board7 = (Button) view.findViewById(R.id.분식) ;
+        Button free_board7 = (Button) view.findViewById(R.id.분식);
         free_board7.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), Button7Activity.class) ;
-                startActivity(intent) ;
+                Intent intent = new Intent(getActivity(), Button7Activity.class);
+                startActivity(intent);
             }
         });
-        Button free_board8 = (Button) view.findViewById(R.id.기타) ;
+        Button free_board8 = (Button) view.findViewById(R.id.기타);
         free_board8.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), Button8Activity.class) ;
-                startActivity(intent) ;
+                Intent intent = new Intent(getActivity(), Button8Activity.class);
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/example/togethersujung2020/ui/ttingBoard/tting_stuff.java
+++ app/src/main/java/com/example/togethersujung2020/ui/ttingBoard/tting_stuff.java
@@ -1,8 +1,11 @@
 package com.example.togethersujung2020.ui.ttingBoard;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,24 +14,25 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.togethersujung2020.R;
 import com.example.togethersujung2020.ui.ttingBoard.dummy.DummyContent;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 /**
  * A fragment representing a list of Items.
  */
 public class tting_stuff extends Fragment {
 
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
-
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
     public tting_stuff() {
     }
 
@@ -37,7 +41,6 @@ public class tting_stuff extends Fragment {
     public static tting_stuff newInstance(int columnCount) {
         tting_stuff fragment = new tting_stuff();
         Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
         fragment.setArguments(args);
         return fragment;
     }
@@ -45,10 +48,6 @@ public class tting_stuff extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
     }
 
     @Override
@@ -56,17 +55,79 @@ public class tting_stuff extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tting_stuff_list, container, false);
 
-        // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+        ListView listview = null;
+        final TtingListViewAdapter adapter;
+
+        // Adapter 생성
+        adapter = new TtingListViewAdapter();
+
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) view.findViewById(R.id.listview1);
+        listview.setAdapter(adapter);
+
+        final DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        Query query = database.child("ttingboard").orderByChild("div").equalTo("공동구매");
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                TtingBoard board = snapshot.getValue(TtingBoard.class);
+                adapter.addItem(board.getTitle(), board.getContent(), board.getDiv(), snapshot.getKey());
+                adapter.notifyDataSetChanged();
             }
-            recyclerView.setAdapter(new MyItemRecyclerViewAdapter(DummyContent.ITEMS));
-        }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                adapter.notifyDataSetChanged();
+            }
+        }); //글 목록 데이터베이스와 리스트뷰 연결
+
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                // get item
+                TtingListViewItem item = (TtingListViewItem) parent.getItemAtPosition(position);
+
+                String titleStr = item.getTitle();
+                String descStr = item.getDesc();
+                String divStr = item.getDiv();
+                String key = item.getKey();
+
+                Intent postview = new Intent(getActivity(), TtingPostViewActivity.class);
+                postview.putExtra("title", titleStr);
+                postview.putExtra("content", descStr);
+                postview.putExtra("div", divStr);
+                postview.putExtra("postkey", key);
+                startActivity(postview);
+            }
+        });
+
+        Button free_board1 = (Button) view.findViewById(R.id.button);
+        free_board1.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newPost = new Intent(getActivity(), TtingWritePostActivity.class);
+                newPost.putExtra("div","공동구매");
+                startActivity(newPost);
+                Toast.makeText(getActivity(), "새 글 등록 버튼을 클릭했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return view;
     }
 }
