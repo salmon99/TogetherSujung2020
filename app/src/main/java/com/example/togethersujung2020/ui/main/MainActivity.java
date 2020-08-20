ce6b25b3dbd9cbaeddad8a0109a9cd68fcd98d30
salmon99 <namhj99@naver.com>
2020-08-21 04:26:49 +0900

    메인화면 아이콘 변경 색 추가 등등

--- app/src/main/java/com/example/togethersujung2020/ui/main/MainActivity.java
+++ app/src/main/java/com/example/togethersujung2020/ui/main/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
